8e275cc2e6a2c656facfc140b32c223cff5a750e
priya2004-git <priya387.sk@gmail.com>
2025-03-06 23:54:26 +0530

    uploading files by me(maha)

---
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,40 @@
+import torch
+import torch.nn as nn
+
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm1d(hidden_size)
+        self.dropout1 = nn.Dropout(0.2)
+
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.dropout2 = nn.Dropout(0.2)
+
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = self.fc3(x)
+        output = self.softmax(x)
+        return output
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return self.softmax(x)
--- model.py
+++ model.py
@@ -2,19 +2,39 @@ import torch
 import torch.nn as nn
 
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
-        self.l2 = nn.Linear(hidden_size, hidden_size) 
-        self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
-    
+class NeuralNetwork(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(NeuralNetwork, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu1 = nn.ReLU()
+        self.bn1 = nn.BatchNorm1d(hidden_size)
+        self.dropout1 = nn.Dropout(0.2)
+
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.relu2 = nn.ReLU()
+        self.bn2 = nn.BatchNorm1d(hidden_size)
+        self.dropout2 = nn.Dropout(0.2)
+
+        self.fc3 = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
-        out = self.l2(out)
-        out = self.relu(out)
-        out = self.l3(out)
-        
-        return out
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.bn1(x)
+        x = self.dropout1(x)
+
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.bn2(x)
+        x = self.dropout2(x)
+
+        x = self.fc3(x)
+        output = self.softmax(x)
+        return output
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return self.softmax(x)
